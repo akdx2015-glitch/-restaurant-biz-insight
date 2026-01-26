c4f9e7c63cd40158ed985e9a26fb58aacaea2ce7
akdx2015-glitch <akdx2015@gmail.com>
2026-01-26 16:39:23 +0900

    Fix build: Downgrade Recharts to v2.15.0 and alias react-is to fix CSS generation

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { resolve } from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      "react-is": resolve(__dirname, "node_modules/react-is"),
+    },
+  },
   build: {
     sourcemap: false,
     chunkSizeWarningLimit: 2000,
